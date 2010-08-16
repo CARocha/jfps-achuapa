732d085bcdb5e9f2a4c658add01150d442165a68
carlos <carcas@sacrac.info>
2010-08-16 13:56:02 -0600

    fix other thing

--- achuapa/encuesta/forms.py
+++ achuapa/encuesta/forms.py
@@ -10,9 +10,7 @@ CHOICE_OPCION_F = (('nada','----'),(1,'Si'),(2,'No'))
 CHOICE_DESDE_F = (('nada','----'),(1,"Menos de 5 año"),(2,"Mas de 5 años"))
 CHOICE_DUENO_F = (('nada','----'),(1,"Hombre"),(2,"Mujer"),(3,"Mancomunado"),(4,"Parientes"),(5,"Colectivo"),(6,"No hay"))
 class AchuapaForm(forms.Form):
-#    cooperativa = forms.ModelChoiceField(required = False, 
-#            queryset=Cooperativa.objects.all())
-    cooperativa = forms.CharField(widget = forms.Select, required = False)
+    #cooperativa = forms.CharField(widget = forms.Select, required = False)
     fecha = forms.ChoiceField(choices=ANOS_CHOICES)
     departamento = forms.ModelChoiceField(queryset=Departamento.objects.all(), 
             required=False, empty_label="Todos los Departamentos")
